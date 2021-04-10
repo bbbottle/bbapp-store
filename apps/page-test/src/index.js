e902eb0c957b2a3f4dbb5cbccab785fe2d03de33
zjhou <z@zjh.im>
2021-04-10 21:56:06 +0800

    feat: add test page

---
+++ apps/page-test/src/index.js
@@ -0,0 +1,8 @@
+import { Unknown } from '@bbbottle/bbicons';
+import { TestPage } from "./test";
+
+export default {
+  title: 'test',
+  icon: Unknown,
+  component: TestPage,
+}
---
+++ apps/page-test/src/test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const TestPage = () => {
+  return (
+    <div>HELLO, This is a test Page.</div>
+  );
+}
