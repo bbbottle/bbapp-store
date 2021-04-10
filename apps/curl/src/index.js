7d914c0bf823132e2914014e8121afe81f03c82b
zjhou <z@zjh.im>
2021-04-10 22:30:58 +0800

    fix: curl syntax check

--- apps/curl/src/index.js
+++ apps/curl/src/index.js
@@ -1,13 +1,17 @@
 export default {
   name: 'curl',
   handler: async (shell, args) => {
+    const err = shell.shell.chalk.red;
     try {
+      if (!args[0]) {
+        await shell.printLine(err('Syntax Error.'));
+        return await shell.printLine('curl <url>')
+      }
       const res = await fetch(args[0]);
       const text = await res.text();
       await shell.print(text);
       await shell.printLine('');
     } catch (e) {
-      const err = shell.shell.chalk.red;
       shell.printLine(err(e.message));
     }
   }
